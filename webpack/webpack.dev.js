7f9f4e06080dd91e7225b7364cdb8af41e020aed
xin.wang <xin_web@sina.com>
2022-10-29 16:03:40 +0800

    feat: 在浏览器显示DevServer的编译错误的信息

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -14,7 +14,8 @@ module.exports = merge(common, {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    overlay: true
   },
   module: {
     rules: [
